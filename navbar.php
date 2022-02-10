03c36e9ea3f445010e54ce3661b24a04de42c83d
incredibledevelopers <56466746+incredibledevelopers@users.noreply.github.com>
2022-02-10 10:33:16 +0530

    Initial Commit

--- navbar.php
+++ navbar.php
@@ -2,18 +2,46 @@
     <div class="container d-flex align-items-center">
 
       <h1 class="logo"><a href="index.php"><img src="assets/img/logo.jpeg" border="0"></a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
       <nav class="nav-menu d-none d-lg-block">
-
-        <ul>
-          <li class="active"><a href="index.php">Home</a></li>
-          <li><a href="aboutus.php">About Us</a></li>
-          <li><a href="services.php">Services</a></li>
-          <li><a href="products.php">Products</a></li>
-          <li><a href="contact.php">Contact Us</a></li>
-
+		<ul>
+			<?php
+		    $pageName = basename($_SERVER['PHP_SELF']);
+			if($pageName == "index.php") {
+			echo "<li class='active'><a href='index.php'>Home</a></li>";
+			echo "<li><a href='aboutus.php'>About Us</a></li>";
+			echo "<li><a href='services.php'>Services</a></li>";
+			echo "<li><a href='products.php'>Products</a></li>";
+			echo "<li><a href='contact.php'>Contact Us</a></li>";
+			}
+			if($pageName == "aboutus.php") {
+			echo "<li><a href='index.php'>Home</a></li>";
+			echo "<li class='active'><a href='aboutus.php'>About Us</a></li>";
+			echo "<li><a href='services.php'>Services</a></li>";
+			echo "<li><a href='products.php'>Products</a></li>";
+			echo "<li><a href='contact.php'>Contact Us</a></li>";
+			}
+			if($pageName == "services.php") {
+			echo "<li><a href='index.php'>Home</a></li>";
+			echo "<li><a href='aboutus.php'>About Us</a></li>";
+			echo "<li class='active'><a href='services.php'>Services</a></li>";
+			echo "<li><a href='products.php'>Products</a></li>";
+			echo "<li><a href='contact.php'>Contact Us</a></li>";
+			}
+			if($pageName == "products.php") {
+			echo "<li><a href='index.php'>Home</a></li>";
+			echo "<li><a href='aboutus.php'>About Us</a></li>";
+			echo "<li><a href='services.php'>Services</a></li>";
+			echo "<li class='active'><a href='products.php'>Products</a></li>";
+			echo "<li><a href='contact.php'>Contact Us</a></li>";
+			}
+			if($pageName == "contact.php") {
+			echo "<li><a href='index.php'>Home</a></li>";
+			echo "<li><a href='aboutus.php'>About Us</a></li>";
+			echo "<li><a href='services.php'>Services</a></li>";
+			echo "<li><a href='products.php'>Products</a></li>";
+			echo "<li class='active'><a href='contact.php'>Contact Us</a></li>";
+			}
+			?>
         </ul>
 
       </nav><!-- .nav-menu -->
